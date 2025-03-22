a8a3575ff5ccbf6b5e9fece94d598ad5df1d808f
emrinasenel <emrinasenel@hotmail.com>
2025-03-22 19:02:09 +0300

    Fitness activity home screen UI & ActivityCard

---
+++ CalorieTracker/Preview Content/ActivityCard.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityCard.swift
+//  CalorieTracker
+//
+//  Created by Emrina Şenel on 22.03.2025.
+//
+
+import SwiftUI
+
+struct ActivityCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityCard()
+}
