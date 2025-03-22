4cb395d708545dd85924e96bbe4500259c6eff21
emrinasenel <emrinasenel@hotmail.com>
2025-03-22 16:35:29 +0300

    Continues

---
+++ CalorieTracker/Preview Content/CalorieTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CalorieTabView.swift
+//  CalorieTracker
+//
+//  Created by Emrina Şenel on 19.03.2025.
+//
+
+import SwiftUI
+
+struct CalorieTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalorieTabView()
+}
